45046ecdc73da0804df7585e9d92fa3e4e1d9d9f
Juan Genovard <juangenovardcaballero@gmail.com>
2022-10-12 20:33:19 +0200

    javascript fucniona pero faltan cosas, todas las paginas creadas

--- js/js.js
+++ js/js.js
@@ -129,170 +129,170 @@
 // ////////////////// Copi por si acaso //////////////
 
 
-const celdas = document.querySelectorAll(".celda");
-const statusText = document.querySelector("#statusText");
-const restart = document.querySelector("#restart");
-const condicionGanador = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-];
+// const celdas = document.querySelectorAll(".celda");
+// const statusText = document.querySelector("#statusText");
+// const restart = document.querySelector("#restart");
+// const condicionGanador = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [3, 6, 9],
+//     [1, 5, 9],
+//     [3, 5, 7],
+// ];
 
-let opciones = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
-let running = false;
+// let opciones = ["", "", "", "", "", "", "", "", ""];
+// let currentPlayer = "X";
+// let running = false;
 
-iniciarJuego();
+// iniciarJuego();
 
-function iniciarJuego(){
-    celdas.forEach(celda => addEventListener("click", clickCelda))
-    restart.addEventListener("click", restartJuergo);
-    statusText.textContent = `${currentPlayer}'s turno`;
-    running = true;
-}
-function clickCelda(){
-const indexCelda = this.getAttribute(indexCelda);
+// function iniciarJuego(){
+//     celdas.forEach(celdas => addEventListener("click", clickCelda))
+//     restart.addEventListener("click", restartJuergo);
+//     statusText.textContent = `${currentPlayer}'s turno`;
+//     running = true;
+// }
+// function clickCelda(){
+// const indexCelda = this.getAttribute(indexCelda);
 
-    if(options[indexCelda] != "" || !running){
-        return;
-    }
+//     if(options[indexCelda] != "" || !running){
+//         return;
+//     }
 
-    updateCelda(this, indexCelda);
-    cambiarJugador();
-    checkWinner();
-}
-function updateCelda(celda, index){
-    options[index] = currentPlayer;
-    celdas.textContent = currentPlayer;
-}
-function cambiarJugador(){
-    currentPlayer = (currentPlayer == "X") ? "O" : "X";
-    statusText.textContent =`${currentPlayer}'s turno`; 
+//     updateCelda(this, indexCelda);
+//     cambiarJugador();
+//     checkWinner();
+// }
+// function updateCelda(celdas, index){
+//     options[index] = currentPlayer;
+//     celdas.textContent = currentPlayer;
+// }
+// function cambiarJugador(){
+//     currentPlayer = (currentPlayer == "X") ? "O" : "X";
+//     statusText.textContent =`${currentPlayer}'s turno`; 
 
-}
-function checkWinner(){
-    rondaGanada = false;
+// }
+// function checkWinner(){
+//     rondaGanada = false;
 
-    for(let i = 0; i < condicionGanador.length; i++){
-        const condition = condicionGanador[i];
-        const celda1 = opciones[condition[0]];
-        const celda2 = opciones[condition[1]];
-        const celda3 = opciones[condition[2]];
+//     for(let i = 0; i < condicionGanador.length; i++){
+//         const condition = condicionGanador[i];
+//         const celda1 = opciones[condition[0]];
+//         const celda2 = opciones[condition[1]];
+//         const celda3 = opciones[condition[2]];
 
-        if(celda1 == "" || celda2 == "" || celda3 == ""){
-            continue;
-        }
-        if(celda1 == celda2  && celda2 == celda3){
-            rondaGanada = true;
-            break;
-        }
-    }
-    if(rondaGanada){
-        statusText.textContent = `${currentPlayer} wins!`;
-        running = false;
-    }
-    else if(opciones.includes("")){
-        statusText.textContent = `Draw`;
-        running = false;
-    }
-    else{
-        cambiarJugador();
-    }
-}
-function restartJuergo(){
-    currentPlayer = "X";
-    opciones = ["", "", "", "", "", "", "", "", ""];
-    statusText.textContent = `${currentPlayer}'s turno`;
-    celdas.forEach(celda => celda.textContent = "");
-    running = true;
-}
+//         if(celda1 == "" || celda2 == "" || celda3 == ""){
+//             continue;
+//         }
+//         if(celda1 == celda2  && celda2 == celda3){
+//             rondaGanada = true;
+//             break;
+//         }
+//     }
+//     if(rondaGanada){
+//         statusText.textContent = `${currentPlayer} wins!`;
+//         running = false;
+//     }
+//     else if(opciones.includes("")){
+//         statusText.textContent = `Draw`;
+//         running = false;
+//     }
+//     else{
+//         cambiarJugador();
+//     }
+// }
+// function restartJuergo(){
+//     currentPlayer = "X";
+//     opciones = ["", "", "", "", "", "", "", "", ""];
+//     statusText.textContent = `${currentPlayer}'s turno`;
+//     celdas.forEach(celdas => celda.textContent = "");
+//     running = true;
+// }
 
 
 ///////////////////// copia de youtube /////////////////
 
 
-// const cells = document.querySelectorAll(".cell");
-// const statusText = document.querySelector("#statusText");
-// const restartBtn = document.querySelector("#restartBtn");
-// const winConditions = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6]
-// ];
-// let options = ["", "", "", "", "", "", "", "", ""];
-// let currentPlayer = "X";
-// let running = false;
+const cells = document.querySelectorAll(".celda");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
 
-// initializeGame();
+initializeGame();
 
-// function initializeGame(){
-//     cells.forEach(cell => cell.addEventListener("click", cellClicked));
-//     restartBtn.addEventListener("click", restartGame);
-//     statusText.textContent = `${currentPlayer}'s turn`;
-//     running = true;
-// }
-// function cellClicked(){
-//     const cellIndex = this.getAttribute("cellIndex");
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
 
-//     if(options[cellIndex] != "" || !running){
-//         return;
-//     }
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
 
-//     updateCell(this, cellIndex);
-//     checkWinner();
-// }
-// function updateCell(cell, index){
-//     options[index] = currentPlayer;
-//     cell.textContent = currentPlayer;
-// }
-// function changePlayer(){
-//     currentPlayer = (currentPlayer == "X") ? "O" : "X";
-//     statusText.textContent = `${currentPlayer}'s turn`;
-// }
-// function checkWinner(){
-//     let roundWon = false;
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+function updateCell(cell, index){  /*-> mi celda , 4*/
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner(){
+    let roundWon = false;
 
-//     for(let i = 0; i < winConditions.length; i++){
-//         const condition = winConditions[i];
-//         const cellA = options[condition[0]];
-//         const cellB = options[condition[1]];
-//         const cellC = options[condition[2]];
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
 
-//         if(cellA == "" || cellB == "" || cellC == ""){
-//             continue;
-//         }
-//         if(cellA == cellB && cellB == cellC){
-//             roundWon = true;
-//             break;
-//         }
-//     }
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
 
-//     if(roundWon){
-//         statusText.textContent = `${currentPlayer} wins!`;
-//         running = false;
-//     }
-//     else if(!options.includes("")){
-//         statusText.textContent = `Draw!`;
-//         running = false;
-//     }
-//     else{
-//         changePlayer();
-//     }
-// }
-// function restartGame(){
-//     currentPlayer = "X";
-//     options = ["", "", "", "", "", "", "", "", ""];
-//     statusText.textContent = `${currentPlayer}'s turn`;
-//     cells.forEach(cell => cell.textContent = "");
-//     running = true;
-// }
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+}
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
+}
